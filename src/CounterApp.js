5eb926d4b291eb7b8f6bf8ae8a5d7ff0e9b5163b
Nick Granados <internick2017@gmail.com>
2021-12-30 14:46:59 -0300

    Finalizar aplicacion de react hook e estado

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,34 @@
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 5 }) => {
+
+    // const [nombre, setNombre] = useState('Goku');
+    // console.log(nombre, setNombre);
+    const [counter, setCounter] = useState(value); // retorna un arreglo con 2 valores
+
+
+    // handleAdd
+    const handleAdd = () => {
+        setCounter(counter + 1)
+        // setCounter((c) => c + 1)
+    }
+    const handleSubstract = () => {
+        setCounter(counter - 1)
+    }
+    const handleReset = () => {
+        setCounter(value)
+    }
+
     return (
 
         <>
             <h1>CounterApp</h1>
-            <h2> {value} </h2>
+            <h2> {counter} </h2>
+
+            <button onClick={handleAdd} >+1</button>
+            <button onClick={handleReset} >Reset</button>
+            <button onClick={handleSubstract} >-1</button>
         </>
 
     );
