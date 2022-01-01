5ca95056c7c82a95232886506e97fd9c8f2fa91e
Nick Granados <internick2017@gmail.com>
2022-01-01 15:16:16 -0300

    probar promesas

---
+++ src/tests/base/09-promesas.test.js
@@ -0,0 +1,26 @@
+import { getHeroeByIdAsync } from "../../base-pruebas/09-promesas";
+import heroes from "../../data/heroes";
+
+describe('Pruebas con promesas', () => {
+    test('deve de retornar un heroe async ', (done) => {
+        const id = 1;
+
+        getHeroeByIdAsync(id)
+            .then(heroe => {
+                expect(heroe).toBe(heroes[0]);
+                done()
+            });
+    })
+
+    test('deve obtener un error si el id del heroe no existe ', (done) => {
+        const id = 10;
+
+        getHeroeByIdAsync(id)
+        .catch(error =>{
+            expect(error).toBe('No se pudo encontrar el héroe');
+            done()
+        })
+    })
+    
+
+})
